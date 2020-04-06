0551c1482f6741bf9d35a07b37ea6b9e9d42095b
Santosh-Devkota <devkota549@gmail.com>
2020-04-06 17:11:17 +0545

    Initial

--- Routes/quiz.js
+++ Routes/quiz.js
@@ -2,7 +2,26 @@ const express = require("express");
 
 const Router = express.Router();
 
+Router.get("/home", (req, res, next) => {
+  console.log("this is get request !");
+});
+Router.post("/home", (req, res, next) => {
+  console.log("this is get request !");
+});
 Router.get("/", (req, res, next) => {
+  console.log("this is get request !");
+});
+
+Router.get("/:id", (req, res, next) => {
+  console.log("this is get request !");
+});
+Router.post("/:id", (req, res, next) => {
+  console.log("this is get request !");
+});
+Router.put("/", (req, res, next) => {
+  console.log("this is get request !");
+});
+Router.delete("/", (req, res, next) => {
   console.log("this is get request !");
 });
 
